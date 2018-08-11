981e58a276217fd2f2a2be8424fb2f17769c9442
Wilton <wiltongraves3.email@gmail.com>
2018-08-11 13:22:19 -0400

    Dope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'profile' => 'pages#profile'
+  devise_for :users
+  get 'about' => 'pages#about'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
